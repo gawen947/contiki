e1d1963f9015b9cbdc0bcb6eed2406248f0895bb
David Hauweele <david@hauweele.net>
2016-04-08 08:26:16 +0200

    Add powercycle states names.

--- tools/monitor/mon-ids.c
+++ tools/monitor/mon-ids.c
@@ -30,6 +30,7 @@
 #include "../../core/mon/context.h"
 #include "../../core/mon/context/control.h"
 #include "../../core/mon/context/mon-ct-radio.h"
+#include "../../core/mon/context/mon-ct-powercycle.h"
 
 /* Macro to ease registering context, state, entities names. */
 #define REG_CTX(context) reg_context_name(MON_CT_ ## context, #context)
@@ -39,6 +40,7 @@
 #define REG_COMMON_ENT(entity) reg_common_entity_name(MON_ENT_ ## entity, #entity)
 
 #define REG_RADIO_ST(state) reg_state_name(MON_CT_RADIO, MON_ST_RADIO_ ## state, #state)
+#define REG_POWERCYCLE_ST(state) reg_state_name(MON_CT_POWERCYCLE, MON_ST_POWERCYCLE_ ## state, #state)
 
 static void register_mon_ids_radio(void)
 {
@@ -55,6 +57,32 @@ static void register_mon_ids_radio(void)
 
   /* Register radio entities names. */
   REG_ENT(MON_CT_RADIO, CC2420);
+}
+
+static void register_mon_ids_powercycle(void)
+{
+  /* Register powercycle states names. */
+  REG_POWERCYCLE_ST(RADIO_ON);
+  REG_POWERCYCLE_ST(RADIO_OFF);
+  REG_POWERCYCLE_ST(RADIO_CCA);
+  REG_POWERCYCLE_ST(RADIO_PENDING);
+  REG_POWERCYCLE_ST(RADIO_RECV);
+  REG_POWERCYCLE_ST(CAN_FASTSLEEP);
+  REG_POWERCYCLE_ST(LISTEN_TIME_AFTER_PACKET_DETECTED);
+  REG_POWERCYCLE_ST(MAX_SILENCE);
+  REG_POWERCYCLE_ST(FASTSLEEP);
+  REG_POWERCYCLE_ST(PACKET_SEEN);
+  REG_POWERCYCLE_ST(SILENCE);
+  REG_POWERCYCLE_ST(ACTIVITY);
+  REG_POWERCYCLE_ST(RECEIVING);
+  REG_POWERCYCLE_ST(PENDING);
+  REG_POWERCYCLE_ST(NEXT_CCA);
+  REG_POWERCYCLE_ST(NEXT_CCA_CHECK);
+  REG_POWERCYCLE_ST(NEXT_POWERCYCLE);
+  REG_POWERCYCLE_ST(ARCH_SLEEP);
+
+  /* Register powercycle entities names. */
+  REG_ENT(MON_CT_POWERCYCLE, CONTIKIMAC);
 }
 
 static void register_mon_ids_control(void)
@@ -81,7 +109,9 @@ void register_mon_ids(void)
   REG_CTX(NETOUT);
   REG_CTX(CPU);
   REG_CTX(RADIO);
+  REG_CTX(POWERCYCLE);
 
   register_mon_ids_control();
   register_mon_ids_radio();
+  register_mon_ids_powercycle();
 }
