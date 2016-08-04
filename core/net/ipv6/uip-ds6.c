268a9aa5e9e912f4534c8860f293660bff111615
David Hauweele <david@hauweele.net>
2016-08-04 06:48:29 +0200

    Return onlink prefix length.

--- core/net/ipv6/uip-ds6.c
+++ core/net/ipv6/uip-ds6.c
@@ -310,7 +310,7 @@ uip_ds6_is_addr_onlink(uip_ipaddr_t *ipaddr)
       locprefix < uip_ds6_prefix_list + UIP_DS6_PREFIX_NB; locprefix++) {
     if(locprefix->isused &&
        uip_ipaddr_prefixcmp(&locprefix->ipaddr, ipaddr, locprefix->length)) {
-      return 1;
+      return locprefix->length; /* FIXME: can we use a prefix of length 0 ? */
     }
   }
   return 0;
