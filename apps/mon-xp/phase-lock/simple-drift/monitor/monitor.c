2146d0b1ab9f49dc9b8ca9fdb53ed5067602162c
David Hauweele <david@hauweele.net>
2017-05-23 17:23:25 +0200

    Add simple-monitor firmware.

---
+++ apps/mon-xp/phase-lock/simple-drift/monitor/monitor.c
@@ -0,0 +1,29 @@
+#include <msp430.h>
+
+#include "memmon.h"
+#include "context.h"
+
+#define BUSY_CYCLES (unsigned int)-1
+
+static void busy()
+{
+  unsigned int n;
+  for(n = BUSY_CYCLES ; n ; n--)
+    __no_operation();
+}
+
+int main()
+{
+  WDTCTL = WDTPW | WDTHOLD; /* stop watchdog timer */
+  P5DIR |= BIT4;            /* configure LED1 (P5.4 on TMote Sky) */
+
+  mon_init(); /* initialize monitor */
+
+  while(1) {
+    P5OUT ^= BIT4; /* blink led */
+    busy();
+    mon_record(MON_CT_CONTROL, 0, 0);
+  }
+
+  return 0;
+}
