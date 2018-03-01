f4e2bbad3d2c066e6ae9cf253ce07e308523fda8
David Hauweele <david@hauweele.net>
2018-03-01 17:31:47 +0100

    Add simple monitor with sleep.

---
+++ apps/mon-xp/phase-lock/simple-drift/monitor-sleep/simple-monitor.c
@@ -0,0 +1,36 @@
+#include <msp430.h>
+
+#include "context/control.h"
+#include "dco-sync.h"
+#include "monitor.h"
+#include "context.h"
+#include "memmon.h"
+
+#define BUSY_CYCLES_B 32
+#define BUSY_CYCLES_A (unsigned int)-1
+
+static void busy()
+{
+  unsigned int n,m;
+  for(m = BUSY_CYCLES_B ; m ; m--) {
+    for(n = BUSY_CYCLES_A ; n ; n--)
+      __no_operation();
+  }
+}
+
+int main()
+{
+  WDTCTL = WDTPW | WDTHOLD; /* stop watchdog timer */
+  P5DIR |= BIT4;            /* configure LED1 (P5.4 on TMote Sky) */
+
+  dco_sync();
+  monitor_init();
+
+  while(1) {
+    P5OUT ^= BIT4; /* blink led */
+    busy();
+    mon_record(MON_CT_CONTROL, MON_ENT_TEST, 1);
+  }
+
+  return 0;
+}
