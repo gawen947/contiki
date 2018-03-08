dd46a15e6d1c59888519a04ca278606fb6bc84e3
David Hauweele <david@hauweele.net>
2018-03-08 18:05:03 +0100

    Use continuous mode with CCR.
    For some reason up mode doesn't work at all.

--- apps/mon-xp/phase-lock/simple-drift/monitor-sleep/simple-monitor.c
+++ apps/mon-xp/phase-lock/simple-drift/monitor-sleep/simple-monitor.c
@@ -6,16 +6,35 @@
 #include "context.h"
 #include "memmon.h"
 
+#ifndef TIMER
+# warning "Default timer interval"
+# define TIMER 32768
+#endif /* TIMER */
+
+/* For some reason up mode (MC0) doesn't work at all.
+   So I use continuous mode (MC1) and reset the counter on each interrupt.
+   see:
+
+   https://github.com/contiki-ng/mspsim/pull/29/commits/0118e6f1e2375e78e41fc0fe16ff26d2b6392007
+ */
+
 /* See msp430/isr_compat.h.
    In this case the MSPGCC way.
    TAIFG is in TIMERA1_VECTOR (not TIMERA0). */
-void __attribute__((interrupt (TIMERA1_VECTOR))) timer_a0(void)
+void __attribute__((interrupt (TIMERA0_VECTOR))) timer_a0(void)
 {
-  /* We need to reset the interrupt or it will be triggered constantly. */
+  /* We need to reset the interrupt or it will be triggered constantly.
+     This is only needed for TIMERA1_VECTOR though. */
+  /*
   volatile unsigned reset_flag = TAIV;
   (void)reset_flag;
+  */
 
   P5OUT ^= BIT4; /* blink led */
+
+  /* we cannot reset the timer with TAR = 0, it does not work,
+     instead with increment it continuously. */
+  TACCR0 += TIMER;
 
   mon_record(MON_CT_CONTROL, MON_ENT_TEST, 1);
 }
@@ -32,16 +51,22 @@ int main()
   dco_sync();
   monitor_init();
 
-  /* Timer configured at ACLK/2 continuous mode
-     should start counting right now. Each interrupt
-     happens every 4 seconds. */
-  TACTL = TACLR | TASSEL0 | ID0 | MC1 | TAIE;
+  TACCR0   = TIMER;
+  TACCR1   = 32768;
+  TACCTL0  = CCIE; /* enable capture/compare interruption */
+  TACTL    = TACLR | TASSEL0 | MC1 /* | TAIE */; /* timer configured at ACLK/1 up mode. */
   _BIS_SR(GIE | SCG0 | SCG1 | CPUOFF); /* LPM3 */
 
   /* We ensure that we are in the sleep mode,
      if we weren't the LED2 would be activated. */
-  while(1)
-    P5OUT &= ~BIT5;
+  while(1) {
+    unsigned int i, j;
+    P5OUT ^= BIT5; /* toggle LED2 */
+    for(j = 0 ; j < 32 ; j++) {
+      for(i = 0 ; i < 32768 ; i++)
+        __no_operation();
+    }
+  }
 
   return 0;
 }
